89b5e9e8ca1c088f260bc39395a5bec28c256fd3
daniel-reason <dreason@live.co.uk>
2017-04-14 10:43:20 +1200

    update user and item tables

---
+++ migrations/20170414101656_add-location-to-item.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('items', function (table) {
+    table.string('location')
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('items', function (table){
+    table.dropColumn('location')
+  })
+};
--- seeds/users.js
+++ seeds/users.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {user_id: 7005, fname: 'Jon',lname: 'Bennett',email: 'jb@email.com',phone: '027 111 2222',location: 'Karori',hash: ''},
-        {user_id: 7006, fname: 'Pou',lname: 'Kupenga',email: 'pk@email.com',phone: '022 333 4444',location: 'Kapiti',hash: ''},
-        {user_id: 7007, fname: 'Dan',lname: 'Blake',email: 'db@email.com',phone: '029 444 5555 ',location: 'Wellington',hash: ''},
-        {user_id: 7008, fname: 'Todd',lname: 'Drinkwater',email: 'td@email.com',phone: '027 555 6666',location: 'Wellington',hash: ''}
+        {user_id: 7005, fname: 'Jon',lname: 'Bennett',email: 'jb@email.com',phone: '027 111 2222', hash: '', address: '8 Drummond Road', suburb: 'Karori', town_city: 'Wellington', postcode: '6023', user_image_url: ''},
+        {user_id: 7006, fname: 'Pou',lname: 'Kupenga',email: 'pk@email.com',phone: '022 333 4444', hash: '', address: '5 Kakapo Street', suburb: 'Paraparaumu', town_city: 'Paraparaumu', postcode: '6050', user_image_url: ''},
+        {user_id: 7007, fname: 'Dan',lname: 'Blake',email: 'db@email.com',phone: '029 444 5555 ', hash: '', address: 'Flat 3/25 Taranaki Street', suburb: 'Wellington', town_city: 'Wellington', postcode: '6021', user_image_url: ''},
+        {user_id: 7008, fname: 'Todd',lname: 'Drinkwater',email: 'td@email.com',phone: '027 555 6666', hash: '', address: '5 Aro Valley', suburb: 'Aro Valley', town_city: 'Wellington', postcode: '6015', user_image_url: ''}
       ]);
     });
 };
