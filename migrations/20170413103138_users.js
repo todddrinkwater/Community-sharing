0a21309bc83d1d24daaf340d863cb9a00b5aad5a
daniel-reason <dreason@live.co.uk>
2017-04-13 13:37:34 +1200

    db tables created and seeded

---
+++ migrations/20170413103138_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('user_id').primary()
+    table.string('fname')
+    table.string('lname')
+    table.string('email')
+    table.string('phone')
+    table.string('location')
+    table.binary('hash')
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
