21af5db9e42e3bd0f78f225aac5f6ac0d93e4002
Pouramua <poukupenga@gmail.com>
2017-04-15 11:31:29 +1200

    not finished yet

--- client/api.js
+++ client/api.js
@@ -2,12 +2,11 @@ const request = require('superagent')
 const url = require('url')
 const config = require('../config')
 
-var listUrl = url.format(config)
-
+var urlPath = url.format(config)
 
 const getListings = (callback) => {
   request
-    .get(listUrl + "/items/")
+    .get(urlPath + "/items/")
     .end(function (err, res) {
       if (err) {
         callback(err)
@@ -17,4 +16,17 @@ const getListings = (callback) => {
     })
 }
 
-module.exports = { getListings }
+const getUsers = (callback, email) => {
+  console.log(email);
+  request
+    .get(urlPath + "/users/" + email)
+    .end(function (err, res) {
+      if (err) {
+        callback(err)
+      } else {
+        callback(null, res.body)
+      }
+    })
+}
+
+module.exports = { getListings, getUsers }
