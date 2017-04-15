fce34458f8379f42ed07990614e60490f6774846
todddrinkwater <todddrinkwaternz@gmail.com>
2017-04-15 17:16:45 +1200

    form data grab - not sending to api just yet

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,26 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { registerUser } from '../api'
+
 const Register = () => (
   <div className="login">
-    <img src="http://otlmedia.co.za/wp-content/uploads/2013/10/share1inch.png" />
+    <img src="http://otlmedia.co.za/wp-content/uploads/2013/10/share1inch.png" method="POST"/>
     <div className="register">
       <h1>Register</h1>
-      <form>
-        <label>First Name: </label><input type="text" className="fname" /><br />
-        <label>Last Name: </label><input type="text" className="lname" />
-        <label>Email: </label><input type="text" className="email" />
-        <label>Address: </label><input type="text" className="address" />
-        <label>Suburb: </label><input type="text" className="suburb" />
-        <label>Town/City: </label><input type="text" className="town-city" />
-        <label>Postcode: </label><input type="text" className="postcode" />
-        <label>Phone Number: </label><input type="text" className="phone" />
-        <label>Password: </label><input type="password" className="hash" />
+      <form onSubmit={ registerNewUser } method="POST">
+        <label>First Name: </label><input type="text" name="fname" /><br />
+        <label>Last Name: </label><input type="text" name="lname"  />
+        <label>Email: </label><input type="text" name="email"  />
+        <label>Address: </label><input type="text" name="address" />
+        <label>Suburb: </label><input type="text" name="suburb" />
+        <label>Town/City: </label><input type="text" name="city" />
+        <label>Postcode: </label><input type="text" name="postcode" />
+        <label>Phone Number: </label><input type="text" name="phone" />
+        <label>Password: </label><input type="password" name="hash" />
         <label>Profile Picture</label><input type="file" name="pic" accept="image/*" />
-        <input type="submit" value="Register" />
+        <input type="submit" value="Register"/>
       </form>
     </div>
   </div>
   )
 
-export default Register
+function registerNewUser(event) {
+  event.preventDefault(event)
+  var formData = {
+   firstName: event.target.elements.fname.value,
+   lastName: event.target.elements.lname.value,
+   email: event.target.elements.email.value,
+   address: event.target.elements.address.value,
+   suburb: event.target.elements.suburb.value,
+   city: event.target.elements.city.value,
+   postcode: event.target.elements.postcode.value,
+   phone: event.target.elements.phone.value
+ }
+  registerUser(testCallback, formData)
+}
+
+testCallBack(null, status){
+  console.log(status)
+}
+
+export Register // Export registerNewUser
