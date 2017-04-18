99b1439467edf462c772ff04c3be2267e4ccb2e0
Pouramua <poukupenga@gmail.com>
2017-04-18 19:07:53 +1200

    Change dashboard to a let

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -6,22 +6,19 @@ import { dashboardTab } from '../actions'
 import MySharingDetails from '../containers/MySharingDetails.jsx'
 import MyProfile from '../containers/MyProfile.jsx'
 
-function Dashboard (props) {
-  return (
-    <div className='dashboard-container'>
-
-      <div className='dashboard-menu-container'>
-        <div id='sharing-button' className='dashboard-button dashboard-active-button' onClick={() => changeTab(props, 'MySharingDetails')}>
-          My Sharing
-        </div>
-        <div id='profile-button' className='dashboard-button' onClick={() => changeTab(props, 'myProfile')} >
-          My Profile
-        </div>
+let Dashboard = (props) => (
+  <div className='dashboard-container'>
+    <div className='dashboard-menu-container'>
+      <div id='sharing-button' className='dashboard-button dashboard-active-button' onClick={() => changeTab(props, 'MySharingDetails')}>
+        My Sharing
       </div>
-      { (props.dashboardState === 'MySharingDetails') ? <MySharingDetails /> : <MyProfile /> }
+      <div id='profile-button' className='dashboard-button' onClick={() => changeTab(props, 'myProfile')} >
+        My Profile
+      </div>
     </div>
-  )
-}
+    { (props.dashboardState === 'MySharingDetails') ? <MySharingDetails /> : <MyProfile /> }
+  </div>
+)
 
 function mapStateToProps (state) {
   return {
@@ -30,8 +27,8 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
-
 function changeTab (props, clickedTab) {
   props.dispatch(dashboardTab(clickedTab))
 }
+
+export default connect(mapStateToProps)(Dashboard)
